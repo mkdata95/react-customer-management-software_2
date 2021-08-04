683b5996d6c34b3cc66fc6e17579a9cedc2fc3bd
mkdata95 <mkdata95@naver.com>
2021-08-04 17:35:58 +0900

    customer create

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import Customer from "./components/Customer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h4>기본 리액트 입니다. </h4>
-      </header>
-    </div>
-  );
+const customer = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "박종훈",
+    birthday: "680826",
+    gender: "남자",
+    job: "자영업",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "조상철",
+    birthday: "680826",
+    gender: "남자",
+    job: "자영업",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "홍길동",
+    birthday: "680826",
+    gender: "남자",
+    job: "자영업",
+  },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customer.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
