1179fa246678dbe9ccbcf43299011b543676c75a
mkdata95 <mkdata95@naver.com>
2021-08-05 13:25:37 +0900

    Create Customer DataTable ,mysql setiing

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "박종훈",
-      birthday: "680826",
-      gender: "남자",
-      job: "자영업",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "조상철",
-      birthday: "680826",
-      gender: "남자",
-      job: "자영업",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "홍길동",
-      birthday: "680826",
-      gender: "남자",
-      job: "자영업",
-    },
-  ]);
-});
+const data = fs.readFileSync("./database.json", "utf8");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
 
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect();
+
+app.get("/api/customers", (req, res) => {
+  connection.query(
+    "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
+    (err, rows, fields) => {
+      res.send(rows);
+      console.log(rows);
+    }
+  );
+});
 app.listen(port, () => console.log(`서버의 동작 포트는 ${port}`));
